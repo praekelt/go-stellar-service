a5f6b927423fffa115244c25a596c79c59675e7c
Yaseen Hamdulay <yaseen@hamdulay.co.za>
2015-06-30 07:21:34 -0400

    retab

--- src/models/wallet.js
+++ src/models/wallet.js
@@ -3,71 +3,71 @@ var Http = require('http');
 var Config = require('../config');
 
 function Wallet(
-	msisdn,
-	private_key,
-	public_key) {
+    msisdn,
+    private_key,
+    public_key) {
 }
 
 var wallet = {
-	create: function(msisdn, pin) {
-		return Promise.resolve(function(resolve, reject) {
-			var wallet_request = Http.request({
-				hostname: Config.WALLET_SERVER,
-				path: '/v1/create',
-			}, 
-			function(res) {
-				res.on('data', function(data) {
-				});
-			});
-		});
-	},
+    create: function(msisdn, pin) {
+        return Promise.resolve(function(resolve, reject) {
+            var wallet_request = Http.request({
+                hostname: Config.WALLET_SERVER,
+                path: '/v1/create',
+            }, 
+            function(res) {
+                res.on('data', function(data) {
+                });
+            });
+        });
+    },
     fetch: function(msisdn, pin) {
         var auth = 'Basic '+new Buffer(msisdn+':'+pin).toString('base64');
-		return new Promise(function(resolve, reject) {
-			var wallet_request = Http.request({
-				hostname: Config.WALLET_SERVER,
+        return new Promise(function(resolve, reject) {
+            var wallet_request = Http.request({
+                hostname: Config.WALLET_SERVER,
                 port: Config.WALLET_PORT,
-				path: '/v1/wallet/'+msisdn,
+                path: '/v1/wallet/'+msisdn,
                 headers: {
                     'Authorization': auth
                 }
-			}, 
-			function(res) {
-				res.on('data', function(data) {
+            }, 
+            function(res) {
+                res.on('data', function(data) {
                     var wallet_data = JSON.parse(data.toString());
                     resolve(wallet_data);
-				});
+                });
                 res.on('error', function(data) {
                     console.error(data);
                     reject(data);
                 });
-			});
+            });
             wallet_request.end();
-		});
+        });
     },
 
     fetchAddress: function(msisdn) {
-		return new Promise(function(resolve, reject) {
-			var wallet_request = Http.request({
-				hostname: Config.WALLET_SERVER,
+        return new Promise(function(resolve, reject) {
+            var wallet_request = Http.request({
+                hostname: Config.WALLET_SERVER,
                 port: Config.WALLET_PORT,
-				path: '/v1/wallet/'+msisdn+'/address',
-			}, 
-			function(res) {
-				res.on('data', function(data) {
+                path: '/v1/wallet/'+msisdn+'/address',
+            }, 
+            function(res) {
+                res.on('data', function(data) {
                     var address_data = JSON.parse(data.toString());
                     if(address_data.error_message) {
                         reject(address_data);
                     } else {
                         resolve(address_data);
                     }
-				});
+                });
                 res.on('error', function(data) {
                     reject(data);
                 });
-			});
+            });
             wallet_request.end();
-		});
+        });
     }
 };
 module.exports = wallet;
